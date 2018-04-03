b1ee43e555e9471e42de5fab7a5e3a9d9fa2cf37
Daniel Stephenson <Danielstephenson24@msn.com>
2018-04-03 17:49:32 +0100

    first day core features

--- ping-pong/src/App.js
+++ ping-pong/src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //set using state to keep track of the players added. 
+    this.state = {
+      players: [],
+      player: "",
+      counter: 0,
+    }
+
+    this.players = this.players.bind (this);
+    this.update = this.update.bind(this);
+    this.shuffles = this.shuffles.bind(this);
+
+  }
+  
+  players() {
+    // If the text field is empty, the player will not be added.
+    if (this.state.player !== "") {
+      
+      const players = this.state.players;
+      players.push(this.state.player);
+      
+      // Sets the text input as blank after add player button is clicked. 
+      this.setState({
+        player: "",
+      })
+    }
+
+    
+  }
+
+  shuffles() {
+    const arr = this.state.players;
+    var i,
+        j,
+        temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    } 
+    this.setState({
+      players: arr,
+    });
+
+}
+
+
+  update(e) {
+    const update = e.target.value;
+    this.setState({
+      player: update,
+    })
+  }
+
   render() {
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> PING PONG </h1>
+        <label htmlFor="players">Enter a player</label>
+        <input id="players" value={ this.state.player } onChange={ this.update }/>
+        <button type="button" onClick={ this.players }> Add player </button>
+        <button id="sort" type="button" onClick={ this.shuffles } > Sort player </button>
+
+      
+        {this.state.players.map((item, index) => {
+                    return (
+                        <li key={item.toString()}> {item} </li>
+                        );
+                })
+                } 
+
       </div>
     );
   }
